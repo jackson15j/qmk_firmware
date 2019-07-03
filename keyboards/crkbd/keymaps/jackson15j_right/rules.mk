f40180ce120bdf0a4e10f32ae929e9237f5f9e4b
Craig Astill <craig.astill@gmail.com>
2019-07-03 22:43:02 +0100

    Removed trackpad from my right-side board

--- keyboards/crkbd/keymaps/jackson15j_right/rules.mk
+++ keyboards/crkbd/keymaps/jackson15j_right/rules.mk
@@ -1,10 +1,7 @@
 # Build Options
-POINTING_DEVICE_ENABLE = yes # Generic Pointer, not as big as mouse keys hopefully.
-MOUSEKEY_ENABLE = yes        # Mouse keys(+4700)
-EXTRAKEY_ENABLE = yes        # Audio control and System control(+450)
-RGBLIGHT_ENABLE = yes        # Enable WS2812 RGB underlight.
+POINTING_DEVICE_ENABLE = no # Generic Pointer, not as big as mouse keys hopefully.
+MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
+EXTRAKEY_ENABLE = no        # Audio control and System control(+450)
+RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 
 BOOTLOADER = atmel-dfu
-
-# Add support for the BB 8520 trackpad
-SRC += trackpad.c
