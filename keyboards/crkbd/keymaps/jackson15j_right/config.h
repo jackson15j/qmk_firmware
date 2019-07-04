4741c7eac1bc8ee4619c723606a4264bd3a944ac
Craig Astill <craig.astill@gmail.com>
2019-07-04 07:22:52 +0100

    Removing code to get h/j column working on right-side.

--- keyboards/crkbd/keymaps/jackson15j_right/config.h
+++ keyboards/crkbd/keymaps/jackson15j_right/config.h
@@ -24,12 +24,3 @@
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
-
-/* key matrix size */
-// Rows are doubled-up
-#undef MATRIX_COL_PINS
-#define MATRIX_COL_PINS { F4, F5, F6, F7, B7, D5 }
-
-/* ws2812 RGB LED */
-#undef RGB_DI_PIN
-#define RGB_DI_PIN B5
