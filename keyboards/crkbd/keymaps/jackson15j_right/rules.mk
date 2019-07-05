195d1994805ba5b2e0d91b8428871bf4f57de3b9
Craig Astill <craig.astill@gmail.com>
2019-07-05 17:26:13 +0100

    Moved to single folder, since my sides are identical.

--- keyboards/crkbd/keymaps/jackson15j_right/rules.mk
+++
@@ -1,7 +0,0 @@
-# Build Options
-POINTING_DEVICE_ENABLE = no # Generic Pointer, not as big as mouse keys hopefully.
-MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
-EXTRAKEY_ENABLE = no        # Audio control and System control(+450)
-RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
-
-BOOTLOADER = atmel-dfu
