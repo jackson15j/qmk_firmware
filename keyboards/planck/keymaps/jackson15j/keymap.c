c69718f285df21097f0412601d7ea1ff549271af
Craig Astill <craig.astill@gmail.com>
2019-08-20 22:45:52 +0100

    Added Planck Keymap to use my user layout
    * Cloned from `planck:default`.
    * Deleted all but Planck's `ADJUST` layer, so I can still play music.
    * Added in my User layouts.
    * Fixed up `#define` to be comma separated.
    * Naive code changes to get planck compiling.
        * TODO: flash and confirm.
    * Moved out `quantum.h` from User header to be just in my `crkbd`
      `config.h` to fix building the planck firmware.

---
+++ keyboards/planck/keymaps/jackson15j/keymap.c
@@ -0,0 +1,204 @@
+/* Copyright 2015-2017 Jack Humbert
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+#include "muse.h"
+#include "jackson15j.h"
+
+extern keymap_config_t keymap_config;
+
+#define LAYOUT_planck_grid_wrapper(...) LAYOUT_planck_grid(__VA_ARGS__)
+// See: `/users/jackson15j/jackson15j.h` for the layouts.
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_QWERTY] = LAYOUT_planck_grid_wrapper(
+    QWERTY_1_12,
+    QWERTY_2_12,
+    QWERTY_3_12_PLANCK,
+    PLANCK_THUMBS
+  ),
+
+  [_SYMBOL] = LAYOUT_planck_grid_wrapper(
+    SYMBOL_1_12,
+    SYMBOL_2_12,
+    SYMBOL_3_12,
+    PLANCK_THUMBS
+  ),
+
+  [_MOVMNT] = LAYOUT_planck_grid_wrapper(
+    MOVMNT_1_12,
+    MOVMNT_2_12,
+    MOVMNT_3_12,
+    PLANCK_THUMBS
+  ),
+
+  [_NUMBS] = LAYOUT_planck_grid_wrapper(
+    NUMBS_1_12,
+    NUMBS_2_12,
+    NUMBS_3_12,
+    PLANCK_THUMBS
+  ),
+
+/* Adjust (Lower + Raise)
+ *                      v------------------------RGB CONTROL--------------------v
+ * ,-----------------------------------------------------------------------------------
+ * |      | Reset|Debug | RGB  |RGBMOD| HUE+ | HUE- | SAT+ | SAT- |BRGTH+|BRGTH-|  Del |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|Plover|      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_ADJUST] = LAYOUT_planck_grid(
+    _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
+    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______,  _______, _______,  _______,
+    _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
+)
+
+};
+
+#ifdef AUDIO_ENABLE
+  float plover_song[][2]     = SONG(PLOVER_SOUND);
+  float plover_gb_song[][2]  = SONG(PLOVER_GOODBYE_SOUND);
+#endif
+
+uint32_t layer_state_set_user(uint32_t state) {
+  return update_tri_layer_state(state, _SYMBOL, _MOVMNT, _ADJUST);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        print("mode just switched to qwerty and this is a huge string\n");
+        set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+      break;
+    case SYMBOL:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_SYMBOL);
+      }
+      return false;
+      break;
+    case MOVMNT:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_MOVMNT);
+      }
+      return false;
+      break;
+    case NUMBS:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_NUMBS);
+      }
+      return false;
+      break;
+  }
+  return true;
+}
+
+bool muse_mode = false;
+uint8_t last_muse_note = 0;
+uint16_t muse_counter = 0;
+uint8_t muse_offset = 70;
+uint16_t muse_tempo = 50;
+
+void encoder_update(bool clockwise) {
+  if (muse_mode) {
+    if (IS_LAYER_ON(_MOVMNT)) {
+      if (clockwise) {
+        muse_offset++;
+      } else {
+        muse_offset--;
+      }
+    } else {
+      if (clockwise) {
+        muse_tempo+=1;
+      } else {
+        muse_tempo-=1;
+      }
+    }
+  } else {
+    if (clockwise) {
+      #ifdef MOUSEKEY_ENABLE
+        tap_code(KC_MS_WH_DOWN);
+      #else
+        tap_code(KC_PGDN);
+      #endif
+    } else {
+      #ifdef MOUSEKEY_ENABLE
+        tap_code(KC_MS_WH_UP);
+      #else
+        tap_code(KC_PGUP);
+      #endif
+    }
+  }
+}
+
+void dip_update(uint8_t index, bool active) {
+  switch (index) {
+    case 0:
+      if (active) {
+        #ifdef AUDIO_ENABLE
+          PLAY_SONG(plover_song);
+        #endif
+        layer_on(_ADJUST);
+      } else {
+        #ifdef AUDIO_ENABLE
+          PLAY_SONG(plover_gb_song);
+        #endif
+        layer_off(_ADJUST);
+      }
+      break;
+    case 1:
+      if (active) {
+        muse_mode = true;
+      } else {
+        muse_mode = false;
+        #ifdef AUDIO_ENABLE
+          stop_all_notes();
+        #endif
+      }
+   }
+}
+
+void matrix_scan_user(void) {
+  #ifdef AUDIO_ENABLE
+    if (muse_mode) {
+      if (muse_counter == 0) {
+        uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
+        if (muse_note != last_muse_note) {
+          stop_note(compute_freq_for_midi_note(last_muse_note));
+          play_note(compute_freq_for_midi_note(muse_note), 0xF);
+          last_muse_note = muse_note;
+        }
+      }
+      muse_counter = (muse_counter + 1) % muse_tempo;
+    }
+  #endif
+}
+
+bool music_mask_user(uint16_t keycode) {
+  switch (keycode) {
+    case SYMBOL:
+    case MOVMNT:
+      return false;
+    default:
+      return true;
+  }
+}
---
+++ keyboards/planck/keymaps/jackson15j/readme.md
@@ -0,0 +1,19 @@
+Jackson15j Planck
+=================
+
+See: [/users/jackson15j/], for layout and generic code.
+
+Build
+-----
+
+From the root of the QMK folder:
+
+```bash
+make plank/rev6:jackson15j:dfu-util
+```
+
+Need to press the `RESET` button on base of the board to put it into Bootloader
+mode.
+
+
+[/users/jackson15j/]: ../../../../users/jackson15j/
