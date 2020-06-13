d927a6d5e923033209759b5a1216c402380de68b
Craig Astill <craig.astill@gmail.com>
2020-06-13 16:03:54 +0100

    Fixed Compilation by moving combo code from header to .c.
    Was having linking issues due to multiple definitions. Fixed by
    moving my combo code to my users `.c` file.

--- users/jackson15j/jackson15j.c
+++ users/jackson15j/jackson15j.c
@@ -1,1 +1,31 @@
 #include "jackson15j.h"
+
+// Colemak combos.
+enum combos {
+    QW_ESC,
+    FP_QUOTE,
+    LU_HYPHEN,
+    YSC_BKSPC,
+    ZX_DEL,
+    CD_TAB,
+    HCOM_UNDSCR,
+    FSSLSH_ENTER
+};
+const uint16_t PROGMEM qw_combo[] = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM fp_combo[] = {KC_F, KC_P, COMBO_END};
+const uint16_t PROGMEM lu_combo[] = {KC_L, KC_U, COMBO_END};
+const uint16_t PROGMEM ysc_combo[] = {KC_Y, KC_SCLN, COMBO_END};
+const uint16_t PROGMEM zx_combo[] = {KC_Z, KC_X, COMBO_END};
+const uint16_t PROGMEM cd_combo[] = {KC_C, KC_D, COMBO_END};
+const uint16_t PROGMEM hcom_combo[] = {KC_H, KC_COMM, COMBO_END};
+const uint16_t PROGMEM fsslsh_combo[] = {KC_DOT, KC_SLASH, COMBO_END};
+combo_t key_combos[COMBO_COUNT] = {
+    [QW_ESC] = COMBO(qw_combo, KC_ESC),
+    [FP_QUOTE] = COMBO(fp_combo, KC_DBLQT),
+    [LU_HYPHEN] = COMBO(lu_combo, KC_QUOT),
+    [YSC_BKSPC] = COMBO(ysc_combo, KC_BSPC),
+    [ZX_DEL] = COMBO(zx_combo, KC_DEL),
+    [CD_TAB] = COMBO(cd_combo, KC_TAB),
+    [HCOM_UNDSCR] = COMBO(hcom_combo, KC_UNDSCR),
+    [FSSLSH_ENTER] = COMBO(fsslsh_combo, KC_ENT)
+};
--- users/jackson15j/jackson15j.h
+++ users/jackson15j/jackson15j.h
@@ -83,35 +83,7 @@ enum userspace_layers {
 #define KC_E_LALT MT(MOD_LALT, KC_E)  // Tap=E, Hold=Left Alt.
 #define KC_I_SHFT MT(MOD_RSFT, KC_I)  // Tap=R, Hold=Right Shift.
 #define KC_O_OS RGUI_T(KC_O)  // Tap=O, Hold=Right OS.
-// Colemak combos.
-enum combos {
-    QW_ESC,
-    FP_QUOTE,
-    LU_HYPHEN,
-    YSC_BKSPC,
-    ZX_DEL,
-    CD_TAB,
-    HCOM_UNDSCR,
-    FSSLSH_ENTER
-};
-const uint16_t PROGMEM qw_combo[] = {KC_Q, KC_W, COMBO_END};
-const uint16_t PROGMEM fp_combo[] = {KC_F, KC_P, COMBO_END};
-const uint16_t PROGMEM lu_combo[] = {KC_L, KC_U, COMBO_END};
-const uint16_t PROGMEM ysc_combo[] = {KC_Y, KC_SCLN, COMBO_END};
-const uint16_t PROGMEM zx_combo[] = {KC_Z, KC_X, COMBO_END};
-const uint16_t PROGMEM cd_combo[] = {KC_C, KC_D, COMBO_END};
-const uint16_t PROGMEM hcom_combo[] = {KC_H, KC_COMM, COMBO_END};
-const uint16_t PROGMEM fsslsh_combo[] = {KC_DOT, KC_SLASH, COMBO_END};
-combo_t key_combos[COMBO_COUNT] = {
-    [QW_ESC] = COMBO(qw_combo, KC_ESC),
-    [FP_QUOTE] = COMBO(fp_combo, KC_DBLQT),
-    [LU_HYPHEN] = COMBO(lu_combo, KC_QUOT),
-    [YSC_BKSPC] = COMBO(ysc_combo, KC_BSPC),
-    [ZX_DEL] = COMBO(zx_combo, KC_DEL),
-    [CD_TAB] = COMBO(cd_combo, KC_TAB),
-    [HCOM_UNDSCR] = COMBO(hcom_combo, KC_UNDSCR),
-    [FSSLSH_ENTER] = COMBO(fsslsh_combo, KC_ENT)
-};
+
 
 #if (!defined(LAYOUT) && defined(KEYMAP))
     #define LAYOUT KEYMAP
